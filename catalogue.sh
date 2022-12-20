cebe1a07f6e472638d72930d0603bd5eebeb92f3
Saipavan k <saipavankolluri43@>
2022-12-20 21:31:28 +0530

    Catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -11,3 +11,9 @@ mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 npm install
 
+sed -i -e 's/MONGO_DNSNAME/mongodb.saipavan1904.online/' systemd.service
+
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl restart catalogue
+systemctl enable catalogue
