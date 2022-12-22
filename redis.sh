fe15618527b74e6a01b9f8fabd6874b81c43a821
Saipavan k <saipavankolluri43@>
2022-12-22 20:34:00 +0530

    User Script

--- redis.sh
+++ redis.sh
@@ -1,9 +1,15 @@
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-dnf module enable redis:remi-6.2 -y
-yum install redis -y
+yum install nodejs
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' etc/redis.conf /etc/redis/redis.conf
-
-
-systemctl enable redis
-systemctl restart redis
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+useradd roboshop
+ ```bash
+curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
+cd /home/roboshop
+unzip /tmp/user.zip
+mv user-main user
+cd /home/roboshop/user
+npm install
+mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+systemctl daemon-reload
+systemctl start user
+systemctl enable user
